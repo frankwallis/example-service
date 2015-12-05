ec93a26edf6ee89fd8a8e7767859064d5000f61d
frankwallis <frankwallis@users.noreply.github.com>
2015-12-05 20:22:09 +0000

    Update example-service.ts

--- example-service.ts
+++ example-service.ts
@@ -4,18 +4,16 @@ import {Injectable} from 'angular2/angular2';
 export class ExampleService {
 
 	constructor() {
-		this.cache = {};//new Map<string, string>();
-
-		//var a = Symbol();
+		this.cache = new Map<string, string>();
 	}
 
-	private cache: any;//Map<string, string>;
+	private cache: Map<string, string>;
 
-	greet(name: string): string {
+	greet(name: string): Promise<string> {
 		name = name || "clint"; // obligatory
 		let greeting = this.cache[name] ? this.cache[name] : "Hello " + name;
 		this.cache[name] = greeting;
 
-		return greeting;
+		return Promise.resolve(greeting);
 	}
 }
