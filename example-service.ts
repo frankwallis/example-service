d52680fba23aad54ebd74d36e03afbeb86e20d71
Frank Wallis <fwallis@outlook.com>
2015-12-26 17:06:51 +0000

    chore(dependencies): upgrade to angular2 beta

--- example-service.ts
+++ example-service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from 'angular2/angular2';
+import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class ExampleService {
@@ -9,11 +9,15 @@ export class ExampleService {
 
 	private cache: Map<string, string>;
 
-	greet(name: string): Promise<string> {
+	greet(name: string): string {
 		name = name || "clint"; // obligatory
 		let greeting = this.cache[name] ? this.cache[name] : "Hello " + name;
 		this.cache[name] = greeting;
 
-		return Promise.resolve(greeting);
+		return greeting;
+	}
+
+	greetAsync(name: string): Promise<string> {
+		return Promise.resolve(this.greet(name));
 	}
 }
